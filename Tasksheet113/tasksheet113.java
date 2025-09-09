1adbeaa3cda6509a2499d081a3b93d01e6475aba
Dominic Calampiano <svfcdomo@gmail.com>
2025-09-09 14:23:04 +0000

    fourth commit

---
+++ Tasksheet113/tasksheet113.java
@@ -0,0 +1,12 @@
+package Tasksheet113;
+
+public class tasksheet113 {
+    public static void main(String[] args) {
+        int check_number = 10;
+        String message;
+
+        for (int i = 1; i<=check_number; i++)
+            System.out.println(message = (i%2==0? i + " is even number" : i + " is odd number"));
+            
+    }
+}
