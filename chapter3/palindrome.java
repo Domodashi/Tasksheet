59bcf26fd2e4ad9d5070d4271e12ef99f0753865
Dominic Calampiano <svfcdomo@gmail.com>
2025-09-16 14:14:24 +0000

    eighth commit

--- chapter3/palindrome.java
+++ chapter3/palindrome.java
@@ -5,13 +5,14 @@ import java.util.Scanner;
 public class palindrome {
     public static void main(String[] args) {
 
-        Scanner input = new Scanner(System.in);
-        System.out.print("Input text: ");
+        Scanner input = new Scanner(System.in); //getting inputed text
+        System.out.print("Input text: ");  
 
-        String output = input.nextLine();
-        StringBuilder routput = new StringBuilder(output).reverse();
-        String revOutput = routput.toString();
+        String output = input.nextLine(); // converting inputed text to string 
+        StringBuilder routput = new StringBuilder(output).reverse(); // converting text to Stringbuilder and reverse it
+        String revOutput = routput.toString(); // convert back string builder to string
 
+        //checking if the inputed text is same with the output
         if(output.equalsIgnoreCase(revOutput))
             System.out.println("This is a palindrome");
 
