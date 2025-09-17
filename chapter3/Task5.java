be7ec70221258ef5c2d76ccf2b0f6886ef62e8ec
Dominic Calampiano <svfcdomo@gmail.com>
2025-09-17 08:47:45 +0000

    ninth commit

---
+++ chapter3/Task5.java
@@ -0,0 +1,27 @@
+package chapter3;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class Task5 {
+    
+    public static void main(String[] args) {
+
+      int size = 3;
+
+        Scanner input = new Scanner(System.in);
+        int [] arraysofnumber = new int[3];
+        System.out.print("Enter 3 numbers: ");
+
+        for (int a = 0; a < arraysofnumber.length; a++)
+          arraysofnumber [a] = input.nextInt();
+          Arrays.sort(arraysofnumber);
+
+
+          if (arraysofnumber[0] == arraysofnumber[1] && arraysofnumber[1] == arraysofnumber[2])
+              System.out.println("All numbers are equal");
+          else
+            System.out.println(arraysofnumber[2]);
+    }
+}
