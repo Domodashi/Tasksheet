b98280f59ab197d61ceefbe8fa255c2ebfe766b2
Dominic Calampiano <svfcdomo@gmail.com>
2025-09-17 12:54:47 +0000

    ninth-sure commit

--- chapter3/Task5.java
+++ chapter3/Task5.java
@@ -5,21 +5,22 @@ import java.util.Scanner;
 
 
 public class Task5 {
-    
-    public static void main(String[] args) {
+        public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
-        int [] arraysofnumber = new int[3];
-        System.out.print("Enter 3 numbers: ");
+        int [] arraysofnumber = new int[3]; // storing the inputed numbers
+        System.out.print("Enter 3 numbers (separated by space or 1 by 1): ");
+        
+        for (int a = 0; a < arraysofnumber.length; a++){
+            arraysofnumber [a] = input.nextInt(); // getting the numbers
+        }
+            
+        Arrays.sort(arraysofnumber); // sorting of numbers
 
-        for (int a = 0; a < arraysofnumber.length; a++)
-          arraysofnumber [a] = input.nextInt();
-          Arrays.sort(arraysofnumber);
 
-
-          if (arraysofnumber[0] == arraysofnumber[1] && arraysofnumber[1] == arraysofnumber[2])
+          if (arraysofnumber[0] == arraysofnumber[1] && arraysofnumber[1] == arraysofnumber[2]) //codition to check if all numbers are equal
               System.out.println("All numbers are equal");
           else
-            System.out.println(arraysofnumber[2]);
+            System.out.println(arraysofnumber[2]); // printing the highest number among 3 numbers entered.
     }
 }
